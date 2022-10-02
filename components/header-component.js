968a0e07a0f7e8b2a026185046b7c80ba5670539
EavOutDom <eavoutdom400@gmail.com>
2022-10-02 18:19:54 +0700

    update navigation

---
+++ components/header-component.js
@@ -0,0 +1,58 @@
+class MyHeader extends HTMLElement {
+    constructor() {
+        super();
+    }
+    connectedCallback() {
+        this.innerHTML = `
+        <header id="header">
+        <nav id="nav">
+            <div class="container">
+                <a class="navItem" href="index.html" id="icon">TheTour</a>
+                <ul class="navList">
+                    <li>
+                        <a class="navItem" href="index.html">Home</a>
+                    </li>
+                    <li>
+                        <a class="navItem" href="province.html">Province</a>
+                    </li>
+                    <li class="navDropdown">
+                        <a class="navItem">About us</a>
+                        <ul class="listDropdown">
+                            <li>
+                                <a href="dom.html">Eav Outdom</a>
+                            </li>
+                            <li>
+                                <a href="virak.html">Chhoeung Chhunvirak</a>
+                            </li>
+                            <li>
+                                <a href="menea.html">Heng Menea</a>
+                            </li>
+                            <li>
+                                <a href="channak.html">Kranh Channak</a>
+                            </li>
+                            <li>
+                                <a href="saran.html">Bronh Saran</a>
+                            </li>
+                            <li>
+                                <a href="theang.html">Yan Theang</a>
+                            </li>
+                            <li>
+                                <a href="kimseng.html">Chey Kimseng</a>
+                            </li>
+                            <li>
+                                <a href="kakada.html">Long Kakda</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a class="navItem" href="contact.html">Contact</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+        </header>
+        `;
+    }
+}
+
+customElements.define("header-component", MyHeader);
--- index.js
+++ index.js
@@ -1,6 +1,19 @@
-console.log("Hello World");
+const activePage = window.location.pathname;
+$(document).ready(function () {
+    $("ul li")
+        .find("a")
+        .each(function () {
+            $(this).toggleClass(
+                "active",
+                $(this).attr("href") == activePage.slice(7)
+            );
+        });
+});
+
 $(document).ready(function () {
     $("h1").click(function () {
         $("h2").hide();
     });
 });
+
+
