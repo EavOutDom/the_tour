8cedfe29c87347406586bb39039d69d028c0af67
EavOutDom <eavoutdom400@gmail.com>
2022-11-01 23:23:01 +0700

    update style and testing javascript

--- src/tourDetail.js
+++ src/tourDetail.js
@@ -5,6 +5,7 @@ const data = [
         id: 1,
         name: 'asdfsdfsd',
         age: 3423434234,
+        Image: [1,2,3,4]
     },
     {
         id: 2,
@@ -26,12 +27,17 @@ const ff = data
     .map((data, index) => {
         if (data.id == id) {
             return `<div>
-            <span>${data.id}</span>
+            <span>${data.id === 1 ? data.id : ''}</span>
             <h1>${data.name}</h1>
             <p>${data.age}</p>
+            <p>${data.Image.map((item, index) => {
+                return `<div style='background-color: black; color:white; margin: 5px'>${item}</div>`
+            }).join('')}</p>
         </div>`;
         }
     })
     .join("");
+
+
 
 detail.innerHTML = ff;
