f7682b0e237c6815006f1efcca173e6b4921b9ee
EavOutDom <eavoutdom400@gmail.com>
2022-11-04 20:20:41 +0700

    update to dynamic

--- src/listTours.js
+++ src/listTours.js
@@ -1,27 +1,24 @@
 import data from "../data/tour.json" assert { type: "json" };
 
-const listTour = document.querySelector("#list_tourx");
-const showHTML = data.tour
-    .map(
-        (data) =>
-            `<a id='${data.id}' class='id' href='./tourDetail.html'>
-            <div class="box_list">
+const listTour = document.querySelector("#list_tour");
+const showList = data.tours.map((tour) => {
+    return `<a href="tourDetail.html" id='${tour.id}' class='id'>
+        <div class="box_list">
             <div class="list_img">
-                <img
-                    alt=""
-                    src="${data.image[0]}"
-                />
-            </div>
-            <div>
-                <h3 class='list_name'>${data.title}</h3>
-                <p class='list_desc'>${data.desc}</p>
-            </div>
+            <img
+                alt=""
+                src="${tour.images[0]}"
+            />
         </div>
-        </a>`
-    )
-    .join("");
+        <div class="ellipse">
+            <h3>${tour.title}</h3>
+            <p>${tour.sub_title}</p>
+        </div>
+    </div>
+</a>`;
+}).join('');
 
-listTour.innerHTML = showHTML;
+listTour.innerHTML = showList;
 
 $(document).ready(function () {
     $(".id").click(function () {
@@ -29,3 +26,31 @@ $(document).ready(function () {
         localStorage.setItem("id", value);
     });
 });
+
+
+// import data from "../data/tour.json" assert { type: "json" };
+
+// const listTour = document.querySelector("#list_tourx");
+// const showHTML = data.tour
+//     .map(
+//         (data) =>
+//             `<a id='${data.id}' class='id' href='./tourDetail.html'>
+//             <div class="box_list">
+//             <div class="list_img">
+//                 <img
+//                     alt=""
+//                     src="${data.image[0]}"
+//                 />
+//             </div>
+//             <div>
+//                 <h3 class='list_name'>${data.title}</h3>
+//                 <p class='list_desc'>${data.desc}</p>
+//             </div>
+//         </div>
+//         </a>`
+//     )
+//     .join("");
+
+// listTour.innerHTML = showHTML;
+
+
--- src/tourDetail.js
+++ src/tourDetail.js
@@ -1,43 +1,118 @@
-import { testData } from "../data.js";
+import data from "../data/tour.json" assert { type: "json" };
+// console.log(data.tours);
 
-const data = [
-    {
-        id: 1,
-        name: 'asdfsdfsd',
-        age: 3423434234,
-        Image: [1,2,3,4]
-    },
-    {
-        id: 2,
-        name: 'asdfadfadfsadfsggkhjfghkg',
-        age: 3423434234,
-    },
-    {
-        id: 3,
-        name: 'akasjdfkdnsdf;l',
-        age: 3423434234,
-    },
-]
+const getId = window.localStorage.getItem("id");
 
-const detail = document.getElementById("detail");
+const filterExistDetail = data.tours.filter(
+    (item) => Number(item.id) !== Number(getId)
+);
+const randomArr = filterExistDetail.sort(() => 0.5 - Math.random());
+const recommendArr = randomArr.slice(0, 4);
 
-const id = window.localStorage.getItem("id");
+const showDetail = data.tours.find((tour) => Number(tour.id) === Number(getId));
+const detail = (document.getElementById("detail").innerHTML = `
+    <section>
+        <p class="txt_title">${showDetail.title}</p>
+        <p class="txt_subtitle">${showDetail.sub_title}</p>
+    </section>
+    <section>
+                <div class="box">
+                    <div class="grid_box">
+                        
+                        <div class="flex_between">
+                            <div
+                                class="swiper mainPic"
+                                style="
+                                    --swiper-navigation-color: #fff;
+                                    --swiper-pagination-color: #fff;
+                                "
+                            >
+                                <div class="swiper-wrapper">
+                                    ${showDetail.images
+                                        .map((img) => {
+                                            return `<div class="swiper-slide">
+                                        <img
+                                            alt=""
+                                            src="${img}"
+                                        />
+                                    </div>`;
+                                        })
+                                        .join("")}
+                                </div>
 
-const ff = data
-    .map((data, index) => {
-        if (data.id == id) {
-            return `<div>
-            <span>${data.id === 1 ? data.id : ''}</span>
-            <h1>${data.name}</h1>
-            <p>${data.age}</p>
-            <p>${data.Image.map((item, index) => {
-                return `<div style='background-color: black; color:white; margin: 5px'>${item}</div>`
-            }).join('')}</p>
-        </div>`;
-        }
-    })
-    .join("");
+                                <div class="swiper-button-next"></div>
+                                <div class="swiper-button-prev"></div>
+                            </div>
+                            <div class="category">
+                                ${showDetail.category
+                                    .map((cate) => `<div>${cate}</div>`)
+                                    .join("")}
+                            </div>
+                        </div>
+                     
 
+                        <div thumbsSlider="" class="swiper thumpPic">
+                        <div class="swiper-wrapper">
+                        ${showDetail.images
+                            .map((img) => {
+                                return `<div class="swiper-slide">
+                            <img
+                                alt=""
+                                src="${img}"
+                            />
+                        </div>`;
+                            })
+                            .join("")}
+                    </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section>
+                <p class="paragraph">
+                    &emsp;&emsp;${showDetail.description}
+                </p>
+            </section>
+            <section class="px-50">
+                <p>. See location on map</p>
+                <div>
+                    <iframe
+                        title="tour"
+                        src="${showDetail.google_map}"
+                        width="100%"
+                        height="450"
+                        style="border: 0; margin-top: 20px; border-radius: 10px"
+                        allowfullscreen=""
+                        loading="lazy"
+                        referrerpolicy="no-referrer-when-downgrade"
+                    ></iframe>
+                </div>
+            </section>
+            <section class="px-50">
+                <p>. Recommend tour</p>
+                <div class="recmd_box">
+                   ${recommendArr.map(recmd => {
+                    return ` <a href="tourDetail.html" class='id' id='${recmd.id}'>
+                    <div class="img_recmd">
+                        <img
+                            alt=""
+                            src="${recmd.images[0]}"
+                        />
+                    </div>
+                    <div>
+                        <p>${recmd.title}</p>
+                        <p>${recmd.sub_title}</p>
+                    </div>
+                </a>    `
+                   }).join('')}              
+                </div>
+            </section>
+`);
 
-
-detail.innerHTML = ff;
+$(document).ready(function () {
+    $(".id").click(function () {
+        let value = $(this).attr("id");
+        localStorage.setItem("id", value);
+        console.log(value)
+    });
+});
